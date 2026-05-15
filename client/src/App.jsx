ee8878155bd81debbd46560829d558cb422bd177
30jpotnuru-lang <30jpotnuru@ojrsd.net>
2026-05-15 12:56:23 +0000

    Add client main setup, styles, contexts and services

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import Landing from './pages/Landing'
 import Login from './pages/auth/Login'
@@ -7,7 +7,6 @@ import Dashboard from './pages/Dashboard'
 import Upload from './pages/Upload'
 import StudySession from './pages/StudySession'
 import { AuthProvider } from './contexts/AuthContext'
-import ProtectedRoute from './components/ProtectedRoute'
 
 const queryClient = new QueryClient()
 
@@ -20,31 +19,9 @@ function App() {
             <Route path="/" element={<Landing />} />
             <Route path="/login" element={<Login />} />
             <Route path="/signup" element={<Signup />} />
-            <Route
-              path="/dashboard"
-              element={
-                <ProtectedRoute>
-                  <Dashboard />
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path="/upload"
-              element={
-                <ProtectedRoute>
-                  <Upload />
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path="/study/:id"
-              element={
-                <ProtectedRoute>
-                  <StudySession />
-                </ProtectedRoute>
-              }
-            />
-            <Route path="*" element={<Navigate to="/" />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/upload" element={<Upload />} />
+            <Route path="/study/:id" element={<StudySession />} />
           </Routes>
         </AuthProvider>
       </Router>
